ebdc086ed5856d328be25316462ad85248478ca7
GingerNinja <louis23412@gmail.com>
2020-02-13 23:01:39 +0200

    changed voting accounts to only trigger once minimum VP has been reached & changed voting weight to scale

--- sneaky-ninja.js
+++ sneaky-ninja.js
@@ -1,5 +1,5 @@
 const steem = require('steem')
-var dsteem = require('dsteem')
+const dsteem = require('dsteem')
 const fs = require('fs');
 const es = require('event-stream')
 const util = require('util')
@@ -43,7 +43,6 @@ const gt = (txt) => {
     return `\x1b[32m${txt}\x1b[0m`
 }
 
-
 const round = (value, decimals) => {
     return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
@@ -74,46 +73,70 @@ const calculateProfit = (arr, avgval) => {
 }
 
 const getVP = async () => {
-    let totalVotePower = 0
     for (userName of userNamesList) {
-        userVotePower = await rcapi.getVPMana(userName)
-        totalVotePower += userVotePower.percentage / 100
+        votingTracker[userName] = await rcapi.getVPMana(userName)
     }
-    return  totalVotePower / userNamesList.length
+
+    for (value of Object.entries(votingTracker)) {
+        if (value[1].percentage / 100 >= MINVOTINGPOWER) {
+            delete offlineVoters[value[0]]
+        } else {
+            delete votingTracker[value[0]]
+            offlineVoters[value[0]] = value[1]
+        }
+    }
+
+    if (Object.values(votingTracker).length > 0) {
+        const newList = Object.values(votingTracker).map(user => {
+            return user.percentage / 100
+        })
+        return Math.max(...newList)
+    } else {
+        const newList = Object.values(offlineVoters).map(user => {
+            return user.percentage / 100
+        })
+        return Math.max(...newList)
+    }
 }
 
-const voteNow = (author, postperm, link, age, blockid, type) => {
-    let buffer = 1
-    if (type === 'comment') {
-        buffer = COMMENTBUFFER
-    }
+const voteNow = (author, postperm, link, age, blockid, type, voteWeight=VOTEWEIGHT, newUserList) => {
+    if (newUserList.length > 0) {
+        let buffer = 1
+        if (type === 'comment') {
+            buffer = COMMENTBUFFER
+        }
 
-    for (user of USERLIST) {
-        const wif = steem.auth.toWif(user[0], user[1], 'posting');
-        steem.broadcast.vote(wif, user[0], author, postperm, VOTEWEIGHT * buffer, (err, result) => {
-            if (err) {
-                if (type === 'post') {
-                    errors++
-                } else if (type === 'comment') {
-                    commentErrors++
-                } else if (type === 'frontrun') {
-                    fr_errors++
+        userToVote = newUserList[0]
+        if (onlineVotersList.includes(userToVote)) {
+            const wif = steem.auth.toWif(userToVote[0], userToVote[1], 'posting');
+            steem.broadcast.vote(wif, userToVote[0], author, postperm, voteWeight * buffer, (err, result) => {
+                if (err) {
+                    if (type === 'post') {
+                        errors++
+                    } else if (type === 'comment') {
+                        commentErrors++
+                    } else if (type === 'frontrun') {
+                        fr_errors++
+                    }
+                    fs.appendFileSync('./errorlog.txt', `\n${err}`)
+                } else {
+                    console.log(gt(`Vote success with a voteweight of ${(voteWeight * buffer) / 100}%!`));
                 }
-                console.log(err)
-                fs.appendFileSync('./errorlog.txt', `\n${err}`)
-            } else {
-                if (type === 'post') {
-                    votes++
-                } else if (type === 'comment') {
-                    commentVotes++
-                } else if (type === 'frontrun') {
-                    fr_votes++
-                }
-                console.log(gt(`Vote Success!`));
-            }
-        });
+            });
+            let updatedUserListToVote = [...newUserList];
+            updatedUserListToVote.splice(0, 1);
+            voteNow(author, postperm, link, age, blockid, type, voteWeight, updatedUserListToVote);
+        }
+    } else if (newUserList.length === 0 ) {
+        fs.appendFileSync('./log.txt', `\nAUTHOR: ${author} -- LINK: ${link} -- DATE: ${new Date()} -- VOTED AFTER: ${age} mins -- Block-Id: ${blockid}\n---------------------------`)
+        if (type === 'post') {
+            votes++
+        } else if (type === 'comment') {
+            commentVotes++
+        } else if (type === 'frontrun') {
+            fr_votes++
+        }
     }
-    fs.appendFileSync('./log.txt', `\nAUTHOR: ${author} -- LINK: ${link} -- DATE: ${new Date()} -- VOTED AFTER: ${age} mins -- Block-Id: ${blockid}\n---------------------------`)
 }
 
 const setSchedule = (time, contentType, author, parentPerm, permLink, avgValue, link, blockId) => {
@@ -147,6 +170,7 @@ const setSchedule = (time, contentType, author, parentPerm, permLink, avgValue, 
                 const nowTime = new Date();
                 const MinuteDiff = ((nowTime - PostCreateDate) / 1000 / 60) - 120
                 const postValue = Number(PostDetails.pending_payout_value.replace(' SBD', ''))
+                const acceptingPayment = Number(PostDetails.max_accepted_payout.replace(' SBD', ''))
                 const totalVoters = PostDetails.active_votes.length
 
                 console.log(`Inspection time for ${yt('@' + author)}!`)
@@ -159,12 +183,16 @@ const setSchedule = (time, contentType, author, parentPerm, permLink, avgValue, 
                     }
                 })
 
-                if (totalVoters <= MAXVOTERS && (postValue / avgValue) <= 0.10 && votesignal === true) {
+                if (totalVoters <= MAXVOTERS && (postValue / avgValue) <= 0.05 && votesignal === true && acceptingPayment > 0) {
+                    let newVoteWeight = Math.round(VOTEWEIGHT * avgValue)
+                    if (newVoteWeight > 10000) {
+                        newVoteWeight = 10000;
+                    }
                     const linkList = link.split('/')
                     const postPerm = linkList[linkList.length -1]
                     console.log(gt(`VOTE OPPORTUNITY DETECTED! Broadcasting now...`))
                     console.log(`---------------------`)
-                    voteNow(author, postPerm, link, MinuteDiff, blockId, contentType);
+                    voteNow(author, postPerm, link, MinuteDiff, blockId, contentType, newVoteWeight, onlineVotersList);
                 } else if (votesignal === false) {
                     console.log(rt(`Already voted here!`))
                 } else {
@@ -197,6 +225,9 @@ let commentVotes = 0
 let errors = 0
 let fr_errors = 0
 let frontRuns = 0
+let votingTracker = {}
+let offlineVoters = {}
+let onlineVotersList = []
 
 console.clear()
 console.log('Starting up block stream...')
@@ -204,14 +235,23 @@ console.log('Starting up block stream...')
 stream.pipe(es.map(async (block, callback) => {
     callback(null, util.inspect(block, {colors: true, depth: null}) + '\n')
 
-    const data = block.transactions
-    const blockId = block.block_id
-    blockCounter ++
-
     const votingPower = await getVP();
     let voteStatus = rt(`Recharging Steem Power...`)
     if (votingPower >= MINVOTINGPOWER) {
         voteStatus = gt('Online!')
+    }
+
+    for (listedVoter of USERLIST) {
+        if (Object.keys(votingTracker).includes(listedVoter[0]) && !onlineVotersList.includes(listedVoter) && 
+        !Object.keys(offlineVoters).includes(listedVoter[0])) {
+            onlineVotersList.push(listedVoter)
+        }
+    }
+
+    for (activeVoter of onlineVotersList) {
+        if (Object.keys(offlineVoters).includes(activeVoter[0])) {
+            onlineVotersList.splice( onlineVotersList.indexOf(activeVoter), 1 );
+        }
     }
 
     const displayTracker = tracker.map(user => {
@@ -222,10 +262,23 @@ stream.pipe(es.map(async (block, callback) => {
         return yt(`@${user}`)
     })
 
-    console.log(`Block-ID: ${yt(blockId)}`)
-    console.log(`Status: ${voteStatus} || Run-time: ${yt(round((new Date() - startTime) / 1000 / 60, 2) + ' mins')} || ${yt(blockCounter)} blocks inspected!`)
-    console.log(`Current-VP: ${yt(round(votingPower, 3) + '%')} || Accounts Voting: ${yt(userNamesList.length)}`)
-    console.log(`Post Votes: ${yt(votes / 3)} || Comment Votes: ${yt(commentVotes / 3)} || Front-Run Votes: ${yt(fr_votes / 3)} || Front-Runs Detected: ${yt(frontRuns)}`)
+    const data = block.transactions
+    const blockId = block.block_id
+    blockCounter ++
+
+    const displayVotingPower = Object.entries(votingTracker).map(acc => {
+        return yt(`@${acc[0]}(${acc[1].percentage / 100}%)`)
+    })
+
+    const displayOfflinePower = Object.entries(offlineVoters).map(acc => {
+        return yt(`@${acc[0]}(${acc[1].percentage / 100}%)`)
+    })
+
+    console.log(`Block-ID: ${yt(blockId)} || ${yt(blockCounter)} blocks inspected!`)
+    console.log(`Status: ${voteStatus} || Run-time: ${yt(round((new Date() - startTime) / 1000 / 60, 2) + ' mins')} || Highest-VP: ${yt(round(votingPower, 3) + '%')}`)
+    console.log(`Accounts online: ${yt(Object.keys(votingTracker).length)}/${yt(userNamesList.length)} ==> [${displayVotingPower}]`)
+    console.log(`Accounts recharging: ${yt(Object.keys(offlineVoters).length)}/${yt(userNamesList.length)} ==> [${displayOfflinePower}]`)
+    console.log(`Post Votes: ${yt(votes)} || Comment Votes: ${yt(commentVotes / userNamesList.length)} || Front-Run Votes: ${yt(fr_votes / userNamesList.length)} || Front-Runs Detected: ${yt(frontRuns)}`)
     console.log(`Post Vote fails: ${yt(errors)} || Comment Vote fails: ${yt(commentErrors)} || Front-Runs Vote Fails: ${yt(fr_errors)}`)
     console.log(`Post-Inspections: ${yt(inspections)} || Pending Post inspections: ${yt(tracker.length)} ==> [${displayTracker}]`)
     console.log(`Comment-Inspections: ${yt(commentInspections)} || Pending Comment inspections: ${yt(commentTracker.length)} ==> [${commentDisplayTracker}]`)
@@ -330,7 +383,7 @@ stream.pipe(es.map(async (block, callback) => {
 
             let votesignal = true
             if (FrontRunCurrentVoters > MAXVOTERS || votingPower < MINVOTINGPOWER 
-                || (FrontRunPostValue / avgValue) > 0.10 || FrontRunAuthorRep < MINREP
+                || (FrontRunPostValue / avgValue) > 0.05 || FrontRunAuthorRep < MINREP
                 || FrontRunMinuteDiff > 2880 || FrontRunMinuteDiff < MINPOSTAGE ) {
                 votesignal = false
             }
@@ -354,7 +407,7 @@ stream.pipe(es.map(async (block, callback) => {
 
             if (votesignal === true) {
                 console.log(gt(`VOTE OPPORTUNITY DETECTED! Broadcasting now...`))
-                voteNow(FrontRunAuthor, FrontRunPerm, FrontRunMemo, FrontRunMinuteDiff, blockId, 'frontrun');
+                voteNow(FrontRunAuthor, FrontRunPerm, FrontRunMemo, FrontRunMinuteDiff, blockId, 'frontrun', newUserList=onlineVotersList);
                 console.log(`---------------------`)
             } else {
                 console.log(rt('Not profitable to vote! :('))
